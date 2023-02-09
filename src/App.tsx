6cd2400f53ba9ff449cb72f9c5e2be560f17554a
Chris McArdle <chrismcardle@hotmail.co.uk>
2023-02-10 11:45:12 +1300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="hero-section">
+        <div className="hero-copy">
+          <h1>Hey there!</h1>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
